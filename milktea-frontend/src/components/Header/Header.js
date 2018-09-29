a4613a270894c8468a81c3cbb5dac92541f6fa8a
Mai Vĩnh Thành <34060324+maivinhthanh@users.noreply.github.com>
2018-09-29 22:48:45 +0700

    fix header

--- milktea-frontend/src/components/Header/Header.js
+++ milktea-frontend/src/components/Header/Header.js
@@ -38,7 +38,7 @@ const MenuLink = ({label, to, active}) =>{
       children ={ ({match}) =>{
         var active = match ? "active" : "";
         return(
-          <li className={"text-menu nav-item active " + {active}}>
+          <li className={"text-menu nav-item " + {active}}>
             <Link to={to} className="nav-link">
               {label}
             </Link>
@@ -75,64 +75,7 @@ class Header extends Component {
             </div>
           </nav>
         </div>
-        <div className="head_content">
-          <div id="mycarousel" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-              <li data-target="#mycarousel" data-slide-to={0} className="active" />
-              <li data-target="#mycarousel" data-slide-to={1} />
-              <li data-target="#mycarousel" data-slide-to={2} />
-            </ol>
-            <div className="carousel-inner" role="listbox">
-              <div className="image carousel-item active">
-                <img src="./img/1-130.jpg" alt="First slide" className="img-carou" />
-                <div className="caption">
-                  <p>Chào mừng đến với quán trà sữa Thành Nhi</p>
-                  <h6>Nơi mà bạn có thể thõa mãn đam mê</h6>
-                  <div className="button input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Tên thức uống..." />
-                    <div className="search input-group-append">
-                      <button name className="btn bg-success">Search</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="image carousel-item">
-                <img src="./img/5.jpg" alt="Second slide" className="img-carou" />
-                <div className="caption">
-                  <p>Công thức mới lạ, đem đến những thức uống mới lạ nhất cho bạn</p>
-                  <h6>Sự lựa chọn đúng đắn</h6>
-                  <div className="button input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Tên thức uống..." />
-                    <div className="search input-group-append">
-                      <button name className="btn bg-success">Search</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="image carousel-item">
-                <img src="./img/service.jpg" alt="Third slide" className="img-carou" />
-                <div className="caption">
-                  <p>Nhiều loại trà sữa khác nhau cho bạn thõa sức lựa chọn</p>
-                  <h6>Đa dạng phong phú</h6>
-                  <div className="button input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Tên thức uống..." />
-                    <div className="search input-group-append">
-                      <button name className="btn bg-success">Tìm kiếm</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <a className="carousel-control-prev" href="#mycarousel" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true" />
-              <span className="sr-only">trước</span>
-            </a>
-            <a className="carousel-control-next" href="#mycarousel" role="button" data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true" />
-              <span className="sr-only">Sau</span>
-            </a>
-          </div>
-        </div>
+        
       </section>
 
     );
